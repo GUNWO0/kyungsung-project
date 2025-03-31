e96eb30ab111e6277b20866fbee1a82bc529531a
GUNWO0 <kk000624@kyungsung.ac.kr>
2025-03-31 21:49:46 +0900

    1주차 개발계í획서

---
+++ src/main/java/com/matacoding/kyungsung01/Kyungsung01Application.java
@@ -0,0 +1,13 @@
+package com.matacoding.kyungsung01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyungsung01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kyungsung01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/matacoding/kyungsung01/Kyungsung01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matacoding.kyungsung01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kyungsung01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
